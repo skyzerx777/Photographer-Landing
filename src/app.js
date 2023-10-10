a1a6ec6e3597eefca1564f3f957d741eebf7b518
Yevhenii Hrebenshchykov <odessa151558@gmail.com>
2023-10-10 18:35:15 +0300

    header

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-import './scss/style.scss'
+import "./scss/style.scss";
+import bootstrap from "bootstrap";
+
+socialLinksHover(".icon_search");
+socialLinksHover(".icon_vk");
+socialLinksHover(".icon_facebook");
+socialLinksHover(".icon_instagram");
+
+function socialLinksHover(socialLink) {
+  let classLink = socialLink.slice(1);
+  document.addEventListener("DOMContentLoaded", () => {
+    let link = document.querySelector(socialLink);
+    link.addEventListener("mouseover", () => {
+      link.classList.toggle(classLink + "-hovered");
+    });
+    link.addEventListener("mouseout", () => {
+      link.classList.toggle(classLink + "-hovered");
+    });
+  });
+}
